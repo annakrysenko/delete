e3bb7eccc73645b932262f1390c8cb823e15edec
Anna Krysenko <krysenkoanna@gmail.com>
2023-08-08 00:08:18 +0300

    k

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,9 +5,6 @@ export const App = () => {
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
       }}
     >
       React homework template
