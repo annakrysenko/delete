6e890cb6d9803e979577e009d579dc69e7a940ea
Anna Krysenko <krysenkoanna@gmail.com>
2023-08-08 00:59:43 +0300

    jj

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,22 @@
+import { useEffect, useState } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+import Layout from './Layout/Layout';
+
+
 export const App = () => {
+  const [firstRenderEnded, setFirstRenderEnded] = useState(false);
+  useEffect(() => {
+    setFirstRenderEnded(true);
+  }, []);
+
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-      }}
-    >
-      React homework template 11
-    </div>
-  );
+    firstRenderEnded && (
+        <Routes>
+          <Route path="/" element={<Layout />}/>
+          <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
+    )
+    )
 };
--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="lot-cryp-dev">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
